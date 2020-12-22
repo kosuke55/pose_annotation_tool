a8837f46f27c273ccf74c432efdf8db9d5a91dad
kosuke55 <kosuke.tnp@gmail.com>
2020-12-22 23:16:48 +0900

    Update make_dir_for_annotation

--- utils/make_dir_for_annotation.py
+++ utils/make_dir_for_annotation.py
@@ -1,13 +1,23 @@
+import argparse
 import os
 import shutil
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-paths = list(Path('./').glob('*/base.obj'))
-out_dir = 'annotation_obj'
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--input-dir', '-i', type=str,
+    default='/media/kosuke55/SANDISK/meshdata/ycb_pouring_object/textured_urdf',  # noqa
+    help='input urdf directory')
+args = parser.parse_args()
+
+input_dir = args.input_dir
+
+paths = list(Path(input_dir).glob('*/base.obj'))
+out_dir = Path(input_dir) / 'annotation_obj'
 os.makedirs(out_dir, exist_ok=True)
 for path in paths:
-    out_path = out_dir / path.parent.with_suffix('.obj')
+    out_path = out_dir / path.parent.with_suffix('.obj').name
     shutil.copy(str(path), str(out_path))
     shutil.copy(str(path.with_suffix('.urdf')),
                 str(out_path.with_suffix('.urdf')))
