d9f2f73f2ab747d86222bdc4fe11dc8e9912421a
kosuke55 <kosuke.tnp@gmail.com>
2020-12-30 18:01:11 +0900

    Add argparse to check_annotation.py

--- utils/check_annotation.py
+++ utils/check_annotation.py
@@ -1,11 +1,29 @@
+import argparse
+
 from pathlib import Path
 from hanging_points_generator.generator_utils \
     import check_contact_points
 
-texture = True
-urdf_dir = '/media/kosuke55/SANDISK/meshdata/ycb_hanging_object/urdf2'
 
-annotation_dir = '/media/kosuke55/SANDISK/meshdata/ycb_hanging_object/urdf2/annotation_obj'  # noqa
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    '--input-urdf', '-u', type=str,
+    help='input urdf directory',
+    default='/media/kosuke55/SANDISK/meshdata/ycb_hanging_object/urdf2')  # noqa
+parser.add_argument(
+    '--input-annotation', '-a', type=str,
+    help='input annotation directory',
+    default='/media/kosuke55/SANDISK/meshdata/ycb_hanging_object/urdf2/annotation_obj')  # noqa
+parser.add_argument(
+    '--texture', type=int,
+    help='if True, load texture.urdf',
+    default=1)
+args = parser.parse_args()
+
+urdf_dir = args.input_urdf
+annotation_dir = args.input_annotation
+texture = args.texture
 
 paths = list(sorted(Path(annotation_dir) .glob('*.json')))
 for path in paths:
